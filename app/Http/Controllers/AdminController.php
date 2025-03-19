719cac9eef4026434359aa45a22fa742ee3dccf9
Hariri99 <hussamhariri99@gmail.com>
2025-03-19 03:22:44 +0200

    Admin edite brand and add the add brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;  
+use Illuminate\Support\Str;  
+use intervention\Image\Laravel\Facades\Image;  
 
 class AdminController extends Controller  
 {  
@@ -19,4 +22,32 @@ class AdminController extends Controller
     public function add_brand(){
         return view('admin.brand-add');
     }
+    public function add_brand_store(Request $request)
+{        
+     $request->validate([
+          'name' => 'required',
+          'slug' => 'required|unique:brands,slug',
+          'image' => 'mimes:png,jpg,jpeg|max:2048'
+     ]);
+
+     $brand = new Brand();
+     $brand->name = $request->name;
+     $brand->slug = Str::slug($request->name);
+     $image = $request->file('image');
+     $file_extention = $request->file('image')->extension();
+     $file_name = Carbon::now()->timestamp . '.' . $file_extention;        
+     $this->GenerateBrandThumbailImage($image,$file_name);
+     $brand->image = $file_name;        
+     $brand->save();
+     return redirect()->route('admin.brands')->with('status','Record has been added successfully !');
+}
+    public function GenerateBrandThumbailImage($image, $imageName){
+       $destinationPath = public_path('uploads/brands');  
+        $img =Image::read($image->path());
+        $img ->cover(124,124,"top");
+        $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath.'/'.$imageName);
+    }
+
 }  
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,6 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth', AuthAdmin::class])->group(function () {  
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');  
     Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');  
-    Route::get('/admin/brands/add', [AdminController::class, 'add_brand'])->name('admin.brands.add');  
+    Route::get('/admin/brand/add',[AdminController::class,'add_brand'])->name('admin.brand.add');
+    Route::post('/admin/brand/store',[AdminController::class,'add_brand_store'])->name('admin.brand.store');
 });  
